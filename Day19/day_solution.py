c7b0937d0aca0467a17b976274e0f3f27e54cad4
Josh Jones <jonesjo7@oregonstate.edu>
2020-12-01 21:54:57 -0800

    added boiler plate for each of the other days

---
+++ Day19/day_solution.py
@@ -0,0 +1,6 @@
+from util import load_and_parse
+
+def parse_function(line):
+    pass
+
+data = load_and_parse("data.txt", parse_function)
--- util.py
+++ util.py
@@ -6,10 +6,14 @@ def load_array(file_name):
 
     return arr
 
-def load_and_parse(file_name: str, parse_func) -> list:
+def default_parse(line):
+    return line
+
+def load_and_parse(file_name: str, parse_func=default_parse) -> list:
     arr = []
     with open(file_name, "r") as infile:
         for line in infile:
             arr.append(parse_func(line))
 
     return arr
+
