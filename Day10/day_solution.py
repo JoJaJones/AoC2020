a0a761542dc2efc61c80a67c9e3c89aa8ae7a785
Josh Jones <jonesjo7@oregonstate.edu>
2020-12-08 21:32:07 -0800

    removed extra days

--- Day10/day_solution.py
+++
@@ -1,6 +0,0 @@
-from util import load_and_parse
-
-def parse_function(line):
-    pass
-
-data = load_and_parse("data.txt", parse_function)
--- util.py
+++ util.py
@@ -6,14 +6,16 @@ def load_array(file_name):
 
     return arr
 
-def default_parse(line, strip_it=True):
+def default_parse_str(line, strip_it=True):
     return line.strip() if strip_it else line
 
-def load_and_parse(file_name: str, parse_func=default_parse) -> list:
+def default_parse_int(line):
+    return int(line)
+
+def load_and_parse(file_name: str, parse_func=default_parse_str) -> list:
     arr = []
     with open(file_name, "r") as infile:
         for line in infile:
             arr.append(parse_func(line))
 
     return arr
-
